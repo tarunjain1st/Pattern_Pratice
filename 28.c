0eac642e6e9184deda86798d104db77de76c5be5
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-20 10:30:07 +0530

    Update 28.c

---
+++ 28.c
@@ -0,0 +1,24 @@
+/*
+   3
+  32
+ 321
+3210
+ 321
+  32
+   3
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i,j,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+
+  for(i=0;i<=2*n+1;i++){
+    for(j=1;j<=abs(n-i);j++){
+      printf(" ");
+    }for(j=n;j>=abs(n-i);j--){
+      printf("%d",j);
+    }printf("\n");
+  }
+}
