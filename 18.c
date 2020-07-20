1d223302da5475b66d47dffac90f440399bed919
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-20 10:00:43 +0530

    Update 18.c

---
+++ 18.c
@@ -0,0 +1,19 @@
+/*
+  1234567
+   12345
+    123
+     1
+*/
+#include <stdio.h>
+int main(){
+  int i,j,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    for(j=1;j<i;j++){
+      printf(" ");
+    }for(j=1;j<=2*(n-i)+1;j++){
+      printf("%d",j);
+    }printf("\n");
+  }
+}
