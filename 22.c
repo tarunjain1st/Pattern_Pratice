a6322b9621c70023cad5fb16c184f400b7460dba
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-20 10:29:47 +0530

    Update 22.c

---
+++ 22.c
@@ -0,0 +1,22 @@
+/*
+    *
+    **
+    ***
+    ****
+    ***
+    **
+    *
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i,j,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+
+  for(i=1;i<=2*n-1;i++){
+    for(j=n;j>abs(n-i);j--){
+      printf("*");
+    }printf("\n");
+  }
+}
