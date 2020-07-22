4494d074819f264069ecb7ae59883b48d55ac14d
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-22 17:05:27 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,22 @@
+/*
+    3
+    23
+    123
+    0123
+    123
+    23
+    3
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i,j,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+
+  for(i=0;i<2*n+1;i++){
+    for(j=abs(n-i);j<=n;j++){
+      printf("%d",j);
+    }printf("\n");
+  }
+}
