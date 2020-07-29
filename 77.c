2f14ff223197144202d31a4ae311fc9c90d48d26
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-29 09:53:42 +0530

    Update 77.c

---
+++ 77.c
@@ -0,0 +1,22 @@
+/*
+            * *
+            * *
+            * * * *
+            * * * *
+            * * * * * *
+            * * * * * *
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i,j,k,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    for(j=1;j<=2;j++){
+      for(k=1;k<=2*i;k++){
+        printf("* ");
+      }printf("\n");
+    }
+  }
+}
