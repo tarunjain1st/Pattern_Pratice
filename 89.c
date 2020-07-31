77581af67891303e462d56a8141e81f975bd6e55
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-07-31 23:02:43 +0530

    Update 89.c

---
+++ 89.c
@@ -0,0 +1,17 @@
+/*
+          *  *  *  *  *  *  *
+          *  *           *  *
+          *     *     *     *
+          *        *        *
+          *     *     *     *
+          *  *           *  *
+          *  *  *  *  *  *  *
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i,j,n;
+  printf("Enter n : ");
+  scanf("%d",&n);
+  
+}
